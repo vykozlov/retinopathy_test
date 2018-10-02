1361e8bf4070db81417313e420e3721b72c43e28
Keiichi Ito <itokeiic@gmail.com>
2018-10-02 11:10:41 +0200

    initial commit

---
+++ retinopathy_test/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Feature building
+"""
+
+# import project config.py
+import retinopathy_test.config as cfg
---
+++ retinopathy_test/visualization/visualize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization
+"""
+# import project config.py
+import retinopathy_test.config as cfg
